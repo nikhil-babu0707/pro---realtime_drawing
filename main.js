69baa91a920ff2130678552d97e734a7d117e4b7
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2022-02-25 16:50:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
+let difference = 0;
+let rightWristX = 0;
+let leftWristX = 0;
+
+
+
 function setup() {
-    video = createCapture(VIDEO);
+    let video = createCapture(VIDEO);
     video.size(550, 500);
 
-    canvas = createCanvas(550, 500);
+    let canvas = createCanvas(550, 500);
     canvas.position(560, 150);
 
-    PoseNet = ml5.poseNet(video, modelloaded);
-    PoseNet.on('pose', gotPoses);
+    let poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelloaded() {
     console.log('PoseNet is Initialized!');
 }
 
-function draw() {
-    background('#969A97');
-}
-
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("left wrist x = " + leftWristX + "right wrist x = " + rightWristX + "difference = " + difference);
     }
+}
+
+function draw() {
+    background('#f2fa0c');
+    document.getElementById("font_size").innerHTML = "font size of the text is : " + difference + "px";
+    textSize(difference);
+    fill('#fa0c0c');
+    text("Hi, I am Nikhil Babu", 50, 400);
 }
